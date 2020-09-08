a5a9bed78a855975f61868ee2effeb74b2b73ad0
potato <1159588554@qq.com>
2020-09-08 09:38:53 +0800

    fisrt commit

---
+++ xc-ui-pc-portal/config/sysConfig.js
@@ -0,0 +1,10 @@
+var sysConfig = {
+    apiURL: '/openapi',//客户端请求
+    staticURL: '/static',
+    backApiURL: 'http://www.xuecheng.com/openapi',//服务端请求
+    backStaticURL: 'http://www.xuecheng.com/static',
+    imgUrl:'http://118.178.193.16',
+    videoUrl:'http://video.xuecheng.com'
+}
+
+module.exports = sysConfig
